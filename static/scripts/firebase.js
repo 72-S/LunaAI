edcd5bfdcb5b68041eead3cf73b82fc946fdcf80
72S <118930716+72-S@users.noreply.github.com>
2023-09-21 18:09:19 +0200

    Update firebase.js

--- static/scripts/firebase.js
+++ static/scripts/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebas
 import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7pYE_d97yWvBFwhuqO1OnfcxRYOtTqnE",
-    authDomain: "shade-example.firebaseapp.com",
-    projectId: "shade-example",
-    storageBucket: "shade-example.appspot.com",
-    messagingSenderId: "465651478438",
-    appId: "1:465651478438:web:ce3d17015847333b222967",
-    measurementId: "G-T4H48EJ8KH",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
